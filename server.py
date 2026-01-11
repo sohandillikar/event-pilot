135fb7f389d1526c807fbff0d6dfab79ef296880
Sohan Dillikar <sohanyt.main@gmail.com>
2026-01-10 18:15:39 -0800

    small thing

--- server.py
+++ server.py
@@ -59,7 +59,7 @@ def negotiate_with_venues(venues: list[dict]):
             customer={"number": venue["contact_phone_number"]},
         )
         print(f"Call created - {call.id}")
-        # time.sleep(150)
+        time.sleep(150)
         return # TODO: Remove this for production
 
 
@@ -100,7 +100,7 @@ def search_venues(event_id: str):
 
     negotiate_with_venues(venues)
 
-    # send_sms(event_id, agent.event.get("customer_phone"))
+    send_sms(event_id, agent.event.get("customer_phone"))
 
 
 @app.get("/get_events")
