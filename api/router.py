018a233ce2041f2f26e2a4923e8a1501f1f0dfaf
Sohan Dillikar <sohanyt.main@gmail.com>
2026-01-25 12:11:01 -0800

    finished with venue search agent

--- api/router.py
+++ api/router.py
@@ -1,15 +1,12 @@
-from fastapi import APIRouter, BackgroundTasks
+from fastapi import APIRouter
 from api.endpoints import (
     event_details
 )
-
-from agents.venue_searching_agent.utils import process_venue_search
 
 router = APIRouter()
 
 router.include_router(event_details.router)
 
 @router.get("/")
-def health_check(background_tasks: BackgroundTasks):
-    background_tasks.add_task(process_venue_search, "e57b1b63-f5e0-403b-aa1b-b16be5703dfa", True)
+def health_check():
     return {"status": "ok"}
